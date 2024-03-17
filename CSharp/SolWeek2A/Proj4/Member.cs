9e4dee1a48f4ae0257f22257d3e3fb18ddfba103
kongmajitenshi <dksk3504@gmail.com>
2024-03-17 21:54:51 +0900

    240317_CSharpAssignment

---
+++ CSharp/SolWeek2A/Proj4/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj4
+{
+    public class Member
+    {
+        public string Name;
+        public int Age;
+        public bool IsRegular;
+
+        public string Status
+        {
+            get
+            {
+                // $"{Name}회원은 {IsRegular ? "정회원":"준회원"}입니다."
+                return $"{Name} 회원은 {(IsRegular ? "정회원":"준회원")}입니다.";
+            }
+        }
+    }
+}
---
+++ CSharp/SolWeek2A/Proj7/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj7
+{
+    public class Employee
+    {
+        public string Name; // 직원이름
+        public string Number; // 사원번호
+        public string Depart; // 부서이름
+        public decimal Salary; // 연봉
+
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return Salary / 12;
+            }
+        }
+    }
+}
